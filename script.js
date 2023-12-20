4c26e53d70809769470dfa23f1d3b0de0936a554
Matthew Bazzo <matthew.bazzo@slalom.com>
2023-12-20 15:32:48 -0500

    feat: implement final game logic for UI

--- script.js
+++ script.js
@@ -14,9 +14,16 @@ const CHOICES_STR = {
 }
 const RND_RSLTS = {
     DRAW : `Draw!`,
-    P1_WIN : 'Player 1 wins!',
-    P2_WIN : 'Player 2 wins!'
+    P1_WIN : 'You win!',
+    P2_WIN : 'Computer wins!'
 }
+const CHOICES_IMG_MAP = {
+    'r' : './rock_choice.png',
+    'p' : './paper_choice.png',
+    's' : './scissors_choice.png'
+}
+const NO_CHOICE_IMG = './no_choice.png'
+const GAME_OVER_MSG = 'Game Over!'
 
 let p1_choice;
 let p2_choice;
@@ -26,28 +33,52 @@ let score = {
     p2_score : 0
 }
 
+// Refernces to DOM nodes
+let p1_choice_img = document.getElementById("p1_choice_img");
+let p2_choice_img = document.getElementById("p2_choice_img");
 let p1_btn_menu = document.getElementById("p1_btn_menu");
+let p2_btn_menu = document.getElementById("p2_btn_menu");
+let p1_btn_shoot = document.getElementById("p1_btn_shoot");
+let p1_score_disp = document.getElementById("p1_score_disp");
+let p2_score_disp = document.getElementById("p2_score_disp");
 let rnd_result = document.getElementById("rnd_result");
-let score_disp = document.getElementById("score_disp");
 
 /**
  * REGISTER EVENT LISTENERS
  */
-
-p1_btn_menu.addEventListener('click', playRound);
+p1_btn_menu.addEventListener('click', getP1Choice);
+p1_btn_shoot.addEventListener('click', playRound);
 
 
 /**
  * SUPORTING FUNCTIONS
  */
-
 function getP1Choice(event) {
     p1_choice = event.target.value;
+    // manage button select state for p1
+    for (const button of event.currentTarget.children) {
+        if (button.id === event.target.id) {
+            button.classList.add("btn-selected");
+        } else {
+            button.classList.remove("btn-selected");
+        }
+    }
+    p1_choice_img.src = CHOICES_IMG_MAP[p1_choice];
+    p2_choice_img.src = NO_CHOICE_IMG;
 }
 
 function getP2Choice() {
     // assign player 2 a random choice
     p2_choice = CHOICES_ARR[Math.floor(Math.random()*CHOICES_ARR.length)];
+    p2_choice_img.src = CHOICES_IMG_MAP[p2_choice];
+    // manage button select state for p2
+    for (const button of p2_btn_menu.children) {
+        if (button.value === p2_choice) {
+            button.classList.add("btn-selected");
+        } else {
+            button.classList.remove("btn-selected");
+        }
+    }
 }
 
 function evalWinner() {
@@ -81,15 +112,15 @@ function evalWinner() {
     return RND_RSLTS.P2_WIN;
 }
 
-function printScore() {
-    console.log(`\nScore:\nPlayer 1 : ${score.p1_score}\nPlayer 2 : ${score.p2_score}`)
-}
-
-function playRound(event) {
-    getP1Choice(event);
+function playRound() {
     getP2Choice();
     rnd_result.textContent =  evalWinner();
-    score_disp.textContent = printScore();
+    p1_score_disp.textContent = score.p1_score;
+    p2_score_disp.textContent = score.p2_score;
+
+    if (score.p1_score === 5 || score.p2_score === 5) {
+        gameOver()
+    }
 }
 
 function resetChoices() {
@@ -98,5 +129,11 @@ function resetChoices() {
 }
 
 function gameOver() {
-    
+    // display game over message
+    rnd_result.textContent =  `${GAME_OVER_MSG} ${
+        score.p1_score > score.p2_score ? RND_RSLTS.P1_WIN :
+            score.p1_score === score.p2_score ? RND_RSLTS.DRAW : RND_RSLTS.P2_WIN}`;
+
+    // disable shoot
+    p1_btn_shoot.disabled = true;
 }
